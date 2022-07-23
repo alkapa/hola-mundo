f9b6fec4f5dc35cb97e5d58ded02b33b2bdcc812
alkapa <juan.camilo.gutierrez.alkapa@gmail.com>
2022-07-22 21:04:21 -0300

    implement: mi cumpleanios feature

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/alkapa/hola-mundo/clima"
 	"github.com/alkapa/hola-mundo/randal"
 )
@@ -12,4 +13,21 @@ func main() {
 	println(
 		randal.ComoEstaElClima(clima.ObtenerEstacionActual()),
 	)
+
+	if randal.EsMiCumpleanios() {
+		fmt.Println("feliz cumpleanios para mi")
+	}
+
+	/*
+		TODO: crear una tipo de dato nuevo llamado Amigo
+		que va a tener 2 campos
+			- Nombre de tipo string
+			- Fecha de Nacimiento de tipo string
+
+		Buscar como crear un type struct para solucionar  esta tarea
+	*/
+
+	if randal.EsTuCumpleanios("27/06/1993") {
+		fmt.Printf("feliz cumple %s", "camilo")
+	}
 }
--- randal/charla.go
+++ randal/charla.go
@@ -1,6 +1,10 @@
 package randal
 
-import "github.com/alkapa/hola-mundo/clima"
+import (
+	"fmt"
+	"github.com/alkapa/hola-mundo/clima"
+	"time"
+)
 
 func Saludo(como string, pregunta string) string {
 	return "Hola " + como + " " + pregunta
@@ -19,14 +23,51 @@ func ComoEstaElClima(estacion clima.Estacion) string {
 	return "Esta normal"
 }
 
-func MiCumpleanios() bool {
-	/*
-		1- definir una constante con la fecha de tu cumpleanios
-		2- usar esa constante e imprimir cuantos dias te faltan para tu proximo cumple
-		3- usar el paquete time para la fecha y sus calculos
-		4- usar una cadena de caracters para la fecha y convertirla en time.Time
-		5- retornar verdadero (true) si el dia de hoy es tu cumpleanios sino retornar falso
-		   (false) eh imprimir cuantos dias faltan
-	*/
-	panic("borrame cuando termines la implementacion")
+func EsMiCumpleanios() bool {
+	const cumpleanios = "02/11/1997"
+	// TODO: agregar constante con tu nombre y pasarla a la nueva firna de la funcion
+	return calculaCumpleanios(cumpleanios)
+}
+
+/*
+	TODO: agregar el nombre de la persona a la firma de la funcion
+*/
+func EsTuCumpleanios(fecha string) bool {
+	return calculaCumpleanios(fecha)
+}
+
+/*
+	TODO: agregar el nombre de la persona a la firma de la funcion
+*/
+func calculaCumpleanios(fecha string) bool {
+	micumpleanios, err := time.Parse("02/01/2006", fecha)
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+
+	diaCumple := micumpleanios.Day()
+	mesCumple := micumpleanios.Month()
+
+	fechaActual := time.Now()
+	fechaActual = time.Date(fechaActual.Year(), fechaActual.Month(), fechaActual.Day(), 0, 0, 0, 0, time.Local)
+
+	anioActual := fechaActual.Year()
+
+	cumpleActual := time.Date(anioActual, mesCumple, diaCumple, 0, 0, 0, 0, time.Local)
+
+	if fechaActual.After(cumpleActual) {
+		cumpleActual = cumpleActual.AddDate(1, 0, 0)
+	}
+
+	diferencia := cumpleActual.Sub(fechaActual)
+	diasRestantes := diferencia.Hours() / 24
+
+	if diferencia != 0 {
+		// TODO: agregar el nombre de la persona y colocarlo dentro del mensaje  para saber a quien le preguntamos
+		fmt.Printf("te faltan %f dias para mi cumpleanios :nombre:", diasRestantes)
+		return false
+	}
+
+	return true
 }
